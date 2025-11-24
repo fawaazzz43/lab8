37b4db4173d6bf7cb03c75987cad44cadb0f017e
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-24 04:09:41 +0200

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,15 +1,46 @@
+package lab7;
 public class Questions {
    
     private String questionText;
     private String correctAnswer;
     private String[] options;
     public int size;
+    private int QuestionsId;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getQuestionsId() {
+        return QuestionsId;
+    }
+
+    public void setQuestionsId(int QuestionsId) {
+        this.QuestionsId = QuestionsId;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
     
-    public Questions( String questionText, String correctAnswer, String[] options) {
-       
+
+    public Questions(String questionText, String correctAnswer, String[] options, int QuestionsId) {
         this.questionText = questionText;
         this.correctAnswer = correctAnswer;
-        this.options = options; 
+        this.options = options;
+        this.QuestionsId = QuestionsId;
     }
     
     public String getQuestionText() {
