b33190c610cd4b4d38840ad4a44cdb87038b5c32
MohamedRabie74 <es.Mohamed.rabea2023@alexu.edu.eg>
2025-11-24 03:12:01 +0200

    Add Certificate part

---
+++ Certificate.java
@@ -0,0 +1,23 @@
+import java.util.UUID;
+
+public class Certificate {
+    private String certificateID;
+    private String studentID;
+    private String courseID;
+    private String courseTitle;
+    private String issueDate;
+
+    public Certificate(String studentID, String courseID, String courseTitle, String issueDate) {
+        this.certificateID = UUID.randomUUID().toString();
+        this.studentID = studentID;
+        this.courseID = courseID;
+        this.courseTitle = courseTitle;
+        this.issueDate = issueDate;
+    }
+
+    public String getCertificateID() { return certificateID; }
+    public String getStudentID() { return studentID; }
+    public String getCourseID() { return courseID; }
+    public String getCourseTitle() { return courseTitle; }
+    public String getIssueDate() { return issueDate; }
+}
