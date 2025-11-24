00ff1c24de69670cd93d675a4c05f013c0ccf4d5
Mahmoud-Elzankalony <mahmoudelzankelwny85@gmail.com>
2025-11-24 04:10:07 +0200

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -1,13 +1,34 @@
+package lab7;
 import java.util.ArrayList;
 
 public class Quiz {
-    String quizID;
-    String title;
-    String numberOfQuestions;
-    ArrayList<Questions> questions;
+    private int quizID;
+    private String title;
+    private int numberOfQuestions;
+    private ArrayList<Questions> questions;
+    private int score;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(int numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
 
     
-    public Quiz(String title, String quizID, String numberOfQuestions) {
+    public Quiz(String title, int quizID, int numberOfQuestions) {
         this.quizID = quizID;
         this.title = title;
         this.numberOfQuestions = numberOfQuestions;
@@ -22,21 +43,21 @@ public class Quiz {
         return questions;
     }
 
-    public String getQuizID() {
+    public int getQuizID() {
         return this.quizID;
     }
 
     public String getQuizTitle() {
         return this.title;
     }
-    public String getnumberOfQuestions() {
+    public int getnumberOfQuestions() {
         return this.numberOfQuestions;
     }
     public boolean isPassed(int score)
     {
         boolean passed=true;
         boolean faild=false;
-        if(score>=Integer.parseInt(this.numberOfQuestions)/2)
+        if(score>=(this.numberOfQuestions)/2)
         {
             return passed;
         }
@@ -44,6 +65,10 @@ public class Quiz {
         {
             return faild;
         }
+    }
+
+    public int getScore() {
+        return score;
     }
 
 
